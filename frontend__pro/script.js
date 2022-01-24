72d90469fe17712dbca4b58fc634f04a5a2f5c40
BoomBunnyBoss <top.evgesha245@gmail.com>
2022-01-24 14:09:38 +0200

    npm init

---
+++ frontend__pro/script.js
@@ -0,0 +1,14 @@
+function Weather (city, data) {
+  this.city = city;
+  this.temp_c = data.temp_c;
+  this.condition = data.condition;
+}
+Weather.prototype.removeClouds = function () {
+  this.condition = 'sun';
+}
+const kyivWeather = new Weather('Kiev', {temp_c: 12, condition: 'clouds'});
+const Lviv = new Weather('Lviv', {temp_c: 7, condition: 'rain'})
+document.body.innerHTML = kyivWeather.temp_c; // выведет в консоль температуру
+console.log(`Температура в ${kyivWeather.city} - ${kyivWeather.temp_c}`);
+kyivWeather.removeClouds(); 
+console.log(kyivWeather.condition) // выведет в консоль sun
